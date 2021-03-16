70f20b2ae6a9db9bffaa8726cb126110fbe1eb8e
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-03-16 14:06:08 -0400

    Added a more extensive docstring for the extract_metadata method and added functionality to extract a dictionary this needs some work due to how it was acomplished

--- SudokuSolver/FileParser.py
+++ SudokuSolver/FileParser.py
@@ -1,6 +1,7 @@
 import os
 from tkinter.filedialog import askopenfilename
 from bs4 import BeautifulSoup as bs
+from soup2dict import convert
 
 
 class FileParser:
@@ -17,37 +18,43 @@ class FileParser:
             raise TypeError('Select an xml file to parse ')
 
     def extract_metadata(self):
-        """Extracts the meta data from the xml file in order to create the puzzle using BeautifulSoup"""
+        """Extracts the meta data from the xml file in order to create the puzzle using BeautifulSoup
+        it creates several class variables:
+        rows_per_box:the number of rows per box
+        cols_per_box:number of columns per box,
+        value_range:the numbers that can be used to fill the boxes
+        start_state:initial state of the puzzle,
+        well_formed:true false or unknown indicates if start_state keys are in range and that it has a correct value_range,
+        solable:indicates if the puzzle is solveable,
+        unique_solution:indicates if there is onle one solution,
+        pigeonhole:indicates if the puzzle is pigeonhole decideable"""
         if self.acceptable_file:
             with open(self.file_name, 'r') as file:
-                content = file.readlines() # extract rows per box 
+                content = file.readlines()  # extract rows per box
                 content = "".join(content)
                 bs_content = bs(content, 'xml')
-                self.rows_per_box = int(bs_content.find("rows_per_box").get_text())
+                self.rows_per_box = int(
+                    bs_content.find("rows_per_box").get_text())
 
-                self.cols_per_box = int(bs_content.find("cols_per_box").get_text())
+                self.cols_per_box = int(
+                    bs_content.find("cols_per_box").get_text())
 
-                self.value_range = range(1,(self.rows_per_box * self.cols_per_box) +1) 
+                self.value_range = range(
+                    1, (self.rows_per_box * self.cols_per_box) + 1)
 
-                #TODO extract start state into a dictionary 
 
-                self.start_state = dict(bs_content.find("start_state").get_text())
+                self.start_state = convert(bs_content.find("start_state"))
 
-                self.well_formed = bool(bs_content.find("well_formed").get_text())
+                self.well_formed = bool(
+                    bs_content.find("well_formed").get_text())
 
                 self.solvable = bool(bs_content.find("solvable").get_text())
 
-                self.unique_solution = bool(bs_content.find("unique_solution").get_text())
+                self.unique_solution = bool(
+                    bs_content.find("unique_solution").get_text())
 
-                self.pigeonhole = bool(bs_content.find("pigeonhole_decidable").get_text())
-
-                print(self.start_state)
-
-               
-
-
-                
-
+                self.pigeonhole = bool(bs_content.find(
+                    "pigeonhole_decidable").get_text())
 
 file = FileParser()
 file.extract_metadata()
