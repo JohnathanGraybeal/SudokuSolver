1bb178033012db5a5d68bb7d51fe163839e13144
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-05-01 15:02:55 -0400

    Add files via upload
    Contains one utility clear screen function

---
+++ SudokuSolver/Utility.py
@@ -0,0 +1,12 @@
+from os import system, name
+
+#Just a utility function to clear output screen
+def clear():
+
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
