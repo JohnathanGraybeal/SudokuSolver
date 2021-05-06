b9d90c96adf0cdda490e6744e532cf3051bd5e58
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-05-06 15:03:59 -0400

    created a proper driver file takes in user input for the file and implements conditional logic to solve arbitrary puzzles

--- SudokuSolver/Main.py
+++ SudokuSolver/Main.py
@@ -1,6 +1,24 @@
 from Solver import *
-import tkinter.filedialog
-# wasn' neccessrily since this program was so messy but it's still better to have it
+from FileParser import *
+from InitializePuzzle import *
+
+
 if __name__ == "__main__":
-    solver = Solver(" ")
-    file = input()
+    print("Enter of the absolute or relative path of the puzzle file you want to use")
+    user_file = input()
+    parser = FileParser(fr"{user_file}")
+    parser.extract_data(fr"{user_file}")
+    parser.display_attributes()
+    puzzle = InitializePuzzle(parser.rows, parser.cols,
+                              parser.value_range, parser.start_state)
+    puzzle.grids
+
+    solve = Solver(puzzle.grids, parser.rows, parser.cols, parser.value_range)
+    solve.display_grid()
+    if parser.rows == 0 and parser.cols == 0:
+        solve.size_zero()
+    elif parser.rows == 0 and parser.cols >= 1:
+        solve.zero_by_n()
+    else:
+        solve.check_cell(puzzle.grids)
+    solve.display_grid()
